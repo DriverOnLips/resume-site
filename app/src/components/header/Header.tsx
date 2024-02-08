e23d57341d6831be115b5c8be8930c9c40724290
Roman Sulaymanov <92111647+DriverOnLips@users.noreply.github.com>
2024-02-08 07:30:00 +0300

    update srcs

--- app/src/components/header/Header.tsx
+++ app/src/components/header/Header.tsx
@@ -3,77 +3,77 @@ import React from 'react';
 import './Header.css';
 
 const Header: React.FC = () => {
-  const scrollToSection = (
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-  ): void => {
-    event.preventDefault();
-    const id = event.currentTarget.getAttribute('href') || '';
-    const section = document.getElementById(id);
-    section?.scrollIntoView({ behavior: 'smooth' });
-  };
+	const scrollToSection = (
+		event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+	): void => {
+		event.preventDefault();
+		const id = event.currentTarget.getAttribute('href') || '';
+		const section = document.getElementById(id);
+		section?.scrollIntoView({ behavior: 'smooth' });
+	};
 
-  return (
-    <header id="header">
-      <div className="header__logo">
-        <a href="intro" onClick={scrollToSection}>
-          <img
-            className="header__logo-img"
-            src="./resume-site/src/assets/img/logo.svg"
-          />
-        </a>
-      </div>
-      <div className="header__btns">
-        <ol className="header__btns__components">
-          <div className="header__btns__components__item header__btns__components__about_me">
-            <a
-              href="about_me"
-              onClick={scrollToSection}
-              className="header__btns__components__item-text text-h5-medium"
-            >
-              Обо мне
-            </a>
-          </div>
-          <div className="header__btns__components__item header__btns__components__tech">
-            <a
-              href="tech"
-              onClick={scrollToSection}
-              className="header__btns__components__item-text text-h5-medium"
-            >
-              Стек технологий
-            </a>
-          </div>
-          <div className="header__btns__components__item header__btns__components__work">
-            <a
-              href="expirience"
-              onClick={scrollToSection}
-              className="header__btns__components__item-text text-h5-medium"
-            >
-              Опыт разработки
-            </a>
-          </div>
-          <div className="header__btns__components__item header__btns__components__contacts">
-            <a
-              href="contact"
-              onClick={scrollToSection}
-              className="header__btns__components__item-text text-h5-medium"
-            >
-              Контакты
-            </a>
-          </div>
-        </ol>
-        <a
-          href="https://t.me/driver_on_lips"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="header__btns__contact_me-href"
-        >
-          <button className="header__btns__contact_me text-h5-medium">
-            Связаться со мной
-          </button>
-        </a>
-      </div>
-    </header>
-  );
+	return (
+		<header id='header'>
+			<div className='header__logo'>
+				<a href='intro' onClick={scrollToSection}>
+					<img
+						className='header__logo-img'
+						src='/resume-site/assets/logo.svg'
+					/>
+				</a>
+			</div>
+			<div className='header__btns'>
+				<ol className='header__btns__components'>
+					<div className='header__btns__components__item header__btns__components__about_me'>
+						<a
+							href='about_me'
+							onClick={scrollToSection}
+							className='header__btns__components__item-text text-h5-medium'
+						>
+							Обо мне
+						</a>
+					</div>
+					<div className='header__btns__components__item header__btns__components__tech'>
+						<a
+							href='tech'
+							onClick={scrollToSection}
+							className='header__btns__components__item-text text-h5-medium'
+						>
+							Стек технологий
+						</a>
+					</div>
+					<div className='header__btns__components__item header__btns__components__work'>
+						<a
+							href='expirience'
+							onClick={scrollToSection}
+							className='header__btns__components__item-text text-h5-medium'
+						>
+							Опыт разработки
+						</a>
+					</div>
+					<div className='header__btns__components__item header__btns__components__contacts'>
+						<a
+							href='contact'
+							onClick={scrollToSection}
+							className='header__btns__components__item-text text-h5-medium'
+						>
+							Контакты
+						</a>
+					</div>
+				</ol>
+				<a
+					href='https://t.me/driver_on_lips'
+					target='_blank'
+					rel='noopener noreferrer'
+					className='header__btns__contact_me-href'
+				>
+					<button className='header__btns__contact_me text-h5-medium'>
+						Связаться со мной
+					</button>
+				</a>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: { port: 3000 },
-  base: 'resume-site',
+  base: '/resume-site',
   plugins: [react()],
 });
