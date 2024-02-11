8863ed7970d663da5c2c6a841cf96e0fd0eb2dde
Roman Sulaymanov <92111647+DriverOnLips@users.noreply.github.com>
2024-02-11 12:30:22 +0300

    update sidebar

--- app/src/components/sidebar/Sidebar.tsx
+++ app/src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,26 @@
 import './Sidebar.css';
 import { scrollToSection } from '../../utils/scrollToSection';
+import { useState } from 'react';
 
 const Sidebar = () => {
+	const [sidebarMode, setSidebarMode] = useState<'opened' | 'closed'>('closed');
+
+	if (sidebarMode === 'closed') {
+		return (
+			<div className='sidebar-closed'>
+				<svg
+					onClick={() => setSidebarMode('opened')}
+					xmlns='http://www.w3.org/2000/svg'
+					height='70'
+					viewBox='0 -960 960 960'
+					width='70'
+				>
+					<path d='M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z' />
+				</svg>
+			</div>
+		);
+	}
+
 	return (
 		<aside id='sidebar'>
 			<div className='sidebar__logo'>
@@ -11,6 +30,16 @@ const Sidebar = () => {
 						src='/resume-site/assets/logo.svg'
 					/>
 				</a>
+				<svg
+					onClick={() => setSidebarMode('closed')}
+					className='sidebar__close-btn'
+					xmlns='http://www.w3.org/2000/svg'
+					height='70'
+					viewBox='0 -960 960 960'
+					width='70'
+				>
+					<path d='m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z' />
+				</svg>
 			</div>
 			<div className='sidebar__btns'>
 				<ol className='sidebar__btns__components'>
